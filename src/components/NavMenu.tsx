919076bde77e9823a9a3c350af231c15a425d42f
PetrHer <petrherynek@seznam.cz>
2023-03-14 23:21:05 +0100

    translated list and NavMenu

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -2,11 +2,37 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 import styles from "../styles/NavMenu.module.css";
 import { api } from "../utils/api";
+import Image from "next/image";
+import PropTypes from 'prop-types';
 
-const NavMenu = () => {
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const NavMenu = ({mainContentLanguage=()=>{}}:{mainContentLanguage:(arg:string)=>void}) => {
   const [selected, setSelected] = useState<boolean>(false);
   const [priv, setPriv] = useState<boolean>(false);
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [showLang, setShowLang] = useState<boolean>(false);
+  const [language, setLanguage] = useState<string>("cz");
+  const [translatedContent, setTranslatedContent] = useState<{
+    home: string;
+    list: string;
+    genealogy: string;
+    detail: string;
+    records: string;
+    login: string;
+    registration: string;
+    signout: string;
+    image:string;
+  }>({
+    home: "ÚVOD",
+    list: "SEZNAM",
+    genealogy: "RODOKMEN",
+    detail: "ÚDAJE",
+    records: "ZÁZNAMY",
+    login: "PŘIHLÁSIT",
+    registration: "REGISTRACE",
+    signout: "Odhlásit",
+    image:'/czech.png'
+  });
   const logout = () => {
     localStorage.removeItem("token");
     window.location.href = "/";
@@ -23,6 +49,10 @@ const NavMenu = () => {
     if (token) {
       verification.mutate(token);
     }
+    const lang = sessionStorage.getItem('lang')
+    if (lang){
+      setLanguage(lang)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useEffect(() => {
@@ -36,38 +66,78 @@ const NavMenu = () => {
       setPriv(privilegesCheck.data.privileges.privileges);
     }
   }, [privilegesCheck.data]);
+
+  useEffect(() => {
+    switch (language) {
+      case "en":
+        setTranslatedContent({
+          home: "HOME",
+          list: "LIST",
+          genealogy: "GENEALOGY",
+          detail: "DETAIL",
+          records: "RECORDS",
+          login: "SIGN IN",
+          registration: "REGISTRATION",
+          signout: "SIGN OUT",
+          image:'/english.png'
+        });
+        break;
+      case "cz":
+        setTranslatedContent({
+          home: "ÚVOD",
+          list: "SEZNAM",
+          genealogy: "RODOKMEN",
+          detail: "ÚDAJE",
+          records: "ZÁZNAMY",
+          login: "PŘIHLÁSIT",
+          registration: "REGISTRACE",
+          signout: "Odhlásit",
+          image:'/czech.png'
+        });
+        break;
+      default:
+        break;
+    }
+  }, [language]);
+
+  const selectLanguage = (lang: string) => {
+    setLanguage(lang);
+    sessionStorage.setItem("lang", lang);
+    mainContentLanguage(lang)
+  };
+
   return (
     <header className={styles.navigation}>
       <>
         <Link className={styles.linkItem} href={"/"}>
-          HOME
+          {translatedContent.home}
         </Link>
         <Link className={styles.linkItem} href={"/list"}>
-          LIST
+          {translatedContent.list}
         </Link>
         {selected && (
           <Link className={styles.linkItem} href={"/genealogy_chart"}>
-            GENEALOGY
+            {translatedContent.genealogy}
           </Link>
         )}
         {selected && verification.isSuccess && (
           <Link className={styles.linkItem} href={"/person_detail"}>
-            DETAIL
+            {translatedContent.detail}
           </Link>
         )}
         {verification.isSuccess && priv && (
           <Link className={styles.linkItem} href={"/records"}>
-            RECORDS
+            {translatedContent.records}
           </Link>
         )}
         {!verification.isSuccess && (
           <Link className={styles.linkItem} href={"/login"}>
-            SIGN IN
+            {translatedContent.login}
           </Link>
         )}
         {!verification.isSuccess && (
           <Link className={styles.linkItem} href={"/registration"}>
-            REGISTRATION
+            {translatedContent.registration}
           </Link>
         )}
         {verification.isSuccess && verification.data && (
@@ -78,15 +148,54 @@ const NavMenu = () => {
           >
             <div className={styles.linkItem}>{verification.data as string}</div>
             {showMenu && (
-              <button className={styles.linkItem} style={{border:"1px solid black"}} onClick={logout}>
-                Sign out
+              <button
+                className={styles.linkItem}
+                style={{ border: "1px solid black" }}
+                onClick={logout}
+              >
+                {translatedContent.signout}
               </button>
             )}
           </div>
         )}
+        <div
+          onMouseEnter={() => setShowLang(true)}
+          onMouseLeave={() => setShowLang(false)}
+        >
+          <Image src={translatedContent.image} alt={""} width={20} height={20} />
+          {showLang && (
+            <>
+              <Image
+                src={"/czech.png"}
+                alt={""}
+                width={20}
+                height={20}
+                onClick={() => selectLanguage("cz")}
+              />
+              <Image
+                src={"/english.png"}
+                alt={""}
+                width={20}
+                height={20}
+                onClick={() => selectLanguage("en")}
+              />
+            </>
+          )}
+        </div>
       </>
     </header>
   );
 };
 
 export default NavMenu;
+
+NavMenu.defaultProps = {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  mainContentLanguage:()=>{},
+
+}
+NavMenu.propTypes = {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+  mainContentLanguage:PropTypes.func,
+
+}
--- src/pages/list.tsx
+++ src/pages/list.tsx
@@ -1,29 +1,61 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import NavMenu from "../components/NavMenu";
 import { api } from "../utils/api";
 
 const Home: NextPage = () => {
   const getData = api.dbRouter.getAll.useMutation();
-  const surname = useRef<HTMLInputElement>(null)
+  const surname = useRef<HTMLInputElement>(null);
+  const [language, setLanguage] = useState<string>("cz");
+  const [translatedContent, setTranslatedContent] = useState<{
+    labelSearch: string;
+    buttonSearch: string;
+  }>({ labelSearch: "Vyhledat podle příjmení", buttonSearch: "Hledat" });
+
   const changeId = (x: number) => {
     sessionStorage.setItem("id", x.toString());
     window.location.href = "/genealogy_chart";
   };
-  const verification = api.authRouter.verify.useMutation()
+  const verification = api.authRouter.verify.useMutation();
   useEffect(() => {
-    const token = localStorage.getItem('token');
-    if (token) { verification.mutate(token) }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+    const token = localStorage.getItem("token");
+    if (token) {
+      verification.mutate(token);
+    }
+    const lang = sessionStorage.getItem('lang')
+    if (lang){
+      setLanguage(lang)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   useEffect(() => {
     getData.mutate();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
+  useEffect(()=>{
+    switch (language) {
+      case 'en':
+        setTranslatedContent({
+          labelSearch:'Search by surname',
+          buttonSearch:'Search'
+        })
+        break;
+        case 'cz':
+          setTranslatedContent({
+            labelSearch:"Vyhledat podle příjmení",
+            buttonSearch:'Hledat'
+          })
+          break;
+      default:
+        break;
+    }
+  },[language])
   const searchBySurname = () => {
-    if (surname.current?.value) { getData.mutate(surname.current.value) }
-  }
+    if (surname.current?.value) {
+      getData.mutate(surname.current.value);
+    }
+  };
   if (getData.isSuccess) {
     getData.data.sort((a, b) => {
       if (a.surname < b.surname) {
@@ -41,19 +73,29 @@ const Home: NextPage = () => {
         <title>Herytage</title>
         <meta name="description" content="heritage of Petr Herynek" />
       </Head>
-      <NavMenu />
-      <main className="mainContent" >
+      <NavMenu mainContentLanguage={(x:string)=>setLanguage(x)} />
+      <main className="mainContent">
         {getData.data &&
           getData.data.map((e) => (
-            <div key={e.id} onClick={() => changeId(e.id)} className="indexDetail">
+            <div
+              key={e.id}
+              onClick={() => changeId(e.id)}
+              className="indexDetail"
+            >
               {e.id} {e.surname} {e.name} {e.year_of_birth}
             </div>
           ))}
         <div className="col-start-3 col-end-4 row-start-1 row-end-4 p-2">
-          <div className='w-40 m-1'>Search by surname : </div>
-          <input className='border border-black w-40 m-1 px-1 rounded-md' ref={surname} type="text" /> 
+          <div className="m-1 w-40">{translatedContent.labelSearch}</div>
+          <input
+            className="m-1 w-40 rounded-md border border-black px-1"
+            ref={surname}
+            type="text"
+          />
           <br />
-          <button onClick={searchBySurname} className='buttons'>Search</button>
+          <button onClick={searchBySurname} className="buttons">
+            {translatedContent.buttonSearch}
+          </button>
         </div>
       </main>
     </>
