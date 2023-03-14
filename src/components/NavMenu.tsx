5dc5acd954d4adf54ac667e44fe6bb313fe7ec1e
PetrHer <petrherynek@seznam.cz>
2023-03-14 20:55:09 +0100

    updated css

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -4,31 +4,38 @@ import styles from "../styles/NavMenu.module.css";
 import { api } from "../utils/api";
 
 const NavMenu = () => {
-  const [selected,setSelected]=useState<boolean>(false)
-  const [priv,setPriv]=useState<boolean>(false)
+  const [selected, setSelected] = useState<boolean>(false);
+  const [priv, setPriv] = useState<boolean>(false);
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   const logout = () => {
     localStorage.removeItem("token");
-    window.location.href = '/';
+    window.location.href = "/";
   };
   const verification = api.authRouter.verify.useMutation();
-  const privilegesCheck = api.authRouter.privilegeCheck.useMutation()
-  useEffect(()=>{
-    if (sessionStorage.getItem('id')){setSelected(true)}
-  },[])
+  const privilegesCheck = api.authRouter.privilegeCheck.useMutation();
+  useEffect(() => {
+    if (sessionStorage.getItem("id")) {
+      setSelected(true);
+    }
+  }, []);
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       verification.mutate(token);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-  useEffect(()=>{
-    if (verification.isSuccess){privilegesCheck.mutate(verification.data as string)}
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  },[verification.data])
-  useEffect(()=>{
-    if (privilegesCheck.data?.privileges?.privileges) {setPriv(privilegesCheck.data.privileges.privileges)}
-  },[privilegesCheck.data])
+  useEffect(() => {
+    if (verification.isSuccess) {
+      privilegesCheck.mutate(verification.data as string);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [verification.data]);
+  useEffect(() => {
+    if (privilegesCheck.data?.privileges?.privileges) {
+      setPriv(privilegesCheck.data.privileges.privileges);
+    }
+  }, [privilegesCheck.data]);
   return (
     <header className={styles.navigation}>
       <>
@@ -38,18 +45,24 @@ const NavMenu = () => {
         <Link className={styles.linkItem} href={"/list"}>
           LIST
         </Link>
-        {selected && (<Link className={styles.linkItem} href={"/genealogy_chart"}>
-          GENEALOGY
-        </Link>)}
-        {selected && verification.isSuccess && (<Link className={styles.linkItem} href={"/person_detail"}>
-          DETAIL
-        </Link>)}
-        {verification.isSuccess &&  priv && (<Link className={styles.linkItem} href={"/records"}>
-          RECORDS
-        </Link>)}
+        {selected && (
+          <Link className={styles.linkItem} href={"/genealogy_chart"}>
+            GENEALOGY
+          </Link>
+        )}
+        {selected && verification.isSuccess && (
+          <Link className={styles.linkItem} href={"/person_detail"}>
+            DETAIL
+          </Link>
+        )}
+        {verification.isSuccess && priv && (
+          <Link className={styles.linkItem} href={"/records"}>
+            RECORDS
+          </Link>
+        )}
         {!verification.isSuccess && (
           <Link className={styles.linkItem} href={"/login"}>
-            LOGIN
+            SIGN IN
           </Link>
         )}
         {!verification.isSuccess && (
@@ -57,8 +70,20 @@ const NavMenu = () => {
             REGISTRATION
           </Link>
         )}
-        {verification.isSuccess && verification.data && (<div className={styles.linkItem}> Logged as {verification.data as string}</div>)}
-        {verification.isSuccess && <button className={styles.linkItem} onClick={logout}>LOGOUT</button>}
+        {verification.isSuccess && verification.data && (
+          <div
+            className={styles.dropDownMenu}
+            onMouseEnter={() => setShowMenu(true)}
+            onMouseLeave={() => setShowMenu(false)}
+          >
+            <div className={styles.linkItem}>{verification.data as string}</div>
+            {showMenu && (
+              <button className={styles.linkItem} style={{border:"1px solid black"}} onClick={logout}>
+                Sign out
+              </button>
+            )}
+          </div>
+        )}
       </>
     </header>
   );
